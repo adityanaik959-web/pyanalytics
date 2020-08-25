f2a56c070e25709c6d04f7209d9920afda4b1d66
Aditya Kumar <70093349+adityanaik959-web@users.noreply.github.com>
2020-08-25 22:06:10 +0530

    Add files via upload

---
+++ daily2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#%%
+#Dictionary - key-value pairs : { curly bracket and colon key:value}
+dict1 = {1:'Ramesh', 2:'Suresh', 3:'Priyanka'}
+dict1
+type(dict1)
+
+car = { 'brand':'Honda', 'model': 'Jazz', 'year' : 2017}
+type(car)
+car
+#access
+car['brand']
+car['year']
+car.get('year')
+dir(car)
+
